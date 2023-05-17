b5affa76b3babf76fe865b87c4a6e28b69f9e422
IntExCZ <55029847+IntExCZ@users.noreply.github.com>
2023-05-17 23:41:30 +0200

    New geocode scannning + debugging
    Completely rewritten geocode scanning mechanism (list/dict area type dependent), added lots of debugging points (messages).

--- python_scripts/chmi_alert.py
+++ python_scripts/chmi_alert.py
@@ -16,7 +16,8 @@ CISORP = "5309" # (string!) vychozi = Pardubice
 if (len(sys.argv) > 1):
 	CISORP = sys.argv[1] # CISORP z command-line parametru
 
-DEBUG = False # vsechny dostupne vystrahy (bez omezeni aktualniho data)
+INCLUDE_EXPIRED = False # zahrnout vystrahy bez omezeni aktualniho data
+DEBUG = False # ladici informace
 
 URL = 'https://www.chmi.cz/files/portal/docs/meteo/om/bulletiny/XOCZ50_OKPR.xml'
 
@@ -63,45 +64,92 @@ severity_int = {
 }
 now = datetime.now().astimezone()
 
+# Hledani geocodu
+def scanGeocode(gcListOrDict):
+	if (isinstance(gcListOrDict, dict)):
+		# slovnik (jeden gc)
+		if (DEBUG):	
+			print(gcListOrDict['value'])
+		if (gcListOrDict['valueName'] == 'CISORP' and gcListOrDict['value'] == CISORP):
+			if (DEBUG):
+				print("^^^^ MATCH!")
+			return True
+	
+	if (isinstance(gcListOrDict, list)):		 
+		# seznam (vice gc)
+		for gc in gcListOrDict:
+			if isinstance(gc, str):
+				if (DEBUG):
+					print(gc + " (invalid, skipped)")
+				continue # nevalidni hodnota
+
+			if (DEBUG):	
+				print(gc['value'])
+			
+			if (gc['valueName'] == 'CISORP' and gc['value'] == CISORP):
+				if (DEBUG):
+					print("^^^^ MATCH!")
+				return True
+		
+	return False
+
 # pruchod pres vystrahy
 try:
 	alerts = []
 	for alert_info in source['alert']['info']:
-		# jazyk hlaseni
+		# jazyk vystrahy
 		if (alert_info['language'] != 'cs'):
-			continue # jen ceska hlaseni
+			continue # jen ceske
 		
-		# druh hlaseni
+		# druh vystrahy
 		if ('responseType' in alert_info and alert_info['responseType'] == 'None'):
-			continue; # jen aktivni hlaseni
+			continue # jen aktivni
 			
-		# datum hlaseni
+		# datum vystrahy
 		expire = datetime.fromisoformat(alert_info['expires'])
-		if (now > expire and not DEBUG):
-			continue; # jen aktualni hlaseni
+		if (now > expire and not INCLUDE_EXPIRED):
+			continue # jen aktualni
+			
+		if (DEBUG):
+			print("Event: " + alert_info['event'])
+			print("Onset: " + alert_info['onset'])
+			print("Expires: " + alert_info['expires'])
+			if (now > expire):
+				print("(expired)")
 
-		# pruchod pres oblasti
+		# prohledani zasazenych kraju
+		if (DEBUG):
+			print("Affected areas with geocodes:")
+
 		cisorp_related = False
 		area_desc = None
-		for area in alert_info['area']:		
-			if isinstance(area, str):
-				continue # nevalidni hodnota
-
+		
+		if (isinstance(alert_info['area'], dict)):
+			# jeden kraj
+			if (DEBUG):
+				print(alert_info['area']['areaDesc'] + " [SINGLE AREA]")
 			# pruchod pres mesta
-			for city in area['geocode']:
-				if isinstance(city, str):
-					continue # nevalidni hodnota
-
-				if (city['valueName'] == 'CISORP' and city['value'] == CISORP):
+			if (scanGeocode(alert_info['area']['geocode'])):
+				cisorp_related = True
+				area_desc = alert_info['area']['areaDesc']; # jakeho kraje e to tyka	
+		
+		if (isinstance(alert_info['area'], list)):
+			# vice kraju
+			for area in alert_info['area']:
+				if (DEBUG):
+					print(area['areaDesc'])			
+				# pruchod pres mesta
+				if (scanGeocode(area['geocode'])):
+					# tyka se mesta
 					cisorp_related = True
-					area_desc = area['areaDesc']; # jakeho kraje se to tyka
-					break # tyka se mesta
+					area_desc = area['areaDesc']; # jakeho kraje e to tyka
+					break
 				
-			if (cisorp_related):
-				break # tyka se oblasti
+		if (DEBUG):
+			print()
 
 		if (not cisorp_related):
-			continue # hlaseni se mesta (= oblasti) netyka
+			continue # hlaseni se mesta (= kraje) netyka
 
 		# udaje vystrahy
 		alert = {
@@ -116,6 +164,8 @@ try:
 			'instructions': alert_info['instruction']
 		}
 		alerts.append(alert) # pridani vystrahy do seznamu vystrah
+		
+		break # prvni shoda nalezena => konec
 
 except:
 	alert = {
